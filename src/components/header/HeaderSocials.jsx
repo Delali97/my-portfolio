5ea7cb44e135bc7b1afd95646c6601eb63396d4b
Delali97 <jennifer.kpolu@yahoo.com>
2022-09-09 00:30:13 -0300

    corrected the linkedin section

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {SiSalesforce} from 'react-icons/si'
 const HeaderSocial = () => {
   return (
     <div className='header__socials'>
-      <a href="linkedin.com/in/jennifer-kpolu/" traget="blank"><BsLinkedin/></a>
+      <a href="https://www.linkedin.com/in/jennifer-kpolu/" traget="blank"><BsLinkedin/></a>
       <a href="https://github.com/Delali97" traget="blank"><FaGithub/></a>
       <a href="https://trailblazer.me/id/jkpolu" traget="blank"><SiSalesforce/></a>
       </div>
