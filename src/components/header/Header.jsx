a43f62f854e2ed78ca58844f0b99f92cd301123c
Delali97 <jennifer.kpolu@yahoo.com>
2022-09-30 00:34:01 -0300

    edited job title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const header = () => {
       <div className='container header__container'>
         <h5>Hello I'm</h5>
         <h1>Jennifer Kpolu</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>Front Developer | Jr Devops Engineer</h5>
         <CTA />
         <HeaderSocial />
         <div className='me'>
